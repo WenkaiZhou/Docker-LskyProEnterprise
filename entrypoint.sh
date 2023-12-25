cec42a94f35f759fd16efcea132de121c2712634
WenkaiZhou <zwenkai@foxmail.com>
2023-12-25 10:49:18 +0800

    Fix .env file permission issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,24 @@
 # terminate on errors
 set -e
 
-ENV_EXAMPLE="/var/www/html/.env.example"
+ENV_FILE="/var/www/html/.env.example"
 
 # Check if volume is empty
 if [ ! -e '/var/www/html/public/index.php' ]; then
     echo 'Setting up lsky-pro volume'
     # Copy lsky-pro from LskyPro src to volume
     cp -a /usr/src/lsky-pro/* /var/www/html/
-    cp -a /usr/src/lsky-pro/.env.example /var/www/html
-    # Configure authorization parameters, which will be copied and initialized internally to .env
-    sed -i 's!APP_URL=.*$!APP_URL='${APP_URL}'!g' ${ENV_EXAMPLE}
-    sed -i 's!APP_SERIAL_NO=.*$!APP_SERIAL_NO='${APP_SERIAL_NO}'!g' ${ENV_EXAMPLE}
-    sed -i 's!APP_SECRET=.*$!APP_SECRET='${APP_SECRET}'!g' ${ENV_EXAMPLE}
+    cp -a /usr/src/lsky-pro/.env.example ${ENV_FILE}
+    # Configure authorization parameters
+    echo 'Configure authorization parameters'
+    sed -i 's!APP_URL=.*$!APP_URL='${APP_URL}'!g' ${ENV_FILE}
+    sed -i 's!APP_SERIAL_NO=.*$!APP_SERIAL_NO='${APP_SERIAL_NO}'!g' ${ENV_FILE}
+    sed -i 's!APP_SECRET=.*$!APP_SECRET='${APP_SECRET}'!g' ${ENV_FILE}
+    chmod www-data:www-data ${ENV_FILE}
 fi
 
-chown -R www-data /var/www/html
-chgrp -R www-data /var/www/html
+# Make sure that the html file is readable by www-data user
+chown -R www-data:www-data /var/www/html/
 chmod -R 755 /var/www/html/
 
 exec "$@"
